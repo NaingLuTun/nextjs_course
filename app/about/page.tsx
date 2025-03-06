09aa1d0f95d77d24c6760e6ec80aa3543debadfe
Naing Lu Tun <nainglu7924@gmail.com>
2025-03-06 13:43:33 +0630

    learned - file structure, client vs server component, routing, dynamic routing

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AboutPage = () => {
+  return (
+    <>
+      <div className='text-xl font-bold'>About Page</div>
+    </>
+    
+  )
+}
+
+export default AboutPage
---
+++ app/components/Hello.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+const Hello = () => {
+    console.log("I am a client component")
+    return (
+        <h1 className="text-xl font-extrabold">Hello from Hello.tsx component</h1>
+    )
+}
+
+export default Hello
